6973eebe6def12faa7d65fd14dce10ff84e02844
Ravish Kumar Tiwari <ravishktiwari@hotmail.com>
2015-01-06 11:10:28 +0530

    committing initial shopping cart structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#Product.delete_all
+Product.create!(title: 'Ruby Gems',
+                description:
+                    %{<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Curabitur in felis ipsum. Aliquam maximus elit aliquet, pellentesque velit in, vulputate metus.
+                    Phasellus rutrum gravida nibh, ac lobortis nibh faucibus vitae.
+                    Sed in consectetur ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                    Aliquam euismod gravida venenatis. Maecenas vitae ligula quis ex porta vehicula. Pellentesque nec iaculis metus.
+                </p>},
+                image_url:   'Ruby-Gem.jpg',
+                price: 16.00)
+# . . .
+Product.create!(title: 'Ruby on Rails AKA ROR',
+                description:
+                    %{<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Curabitur in felis ipsum. Aliquam maximus elit aliquet, pellentesque velit in, vulputate metus.
+                    Phasellus rutrum gravida nibh, ac lobortis nibh faucibus vitae.
+                    Sed in consectetur ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                    Aliquam euismod gravida venenatis. Maecenas vitae ligula quis ex porta vehicula. Pellentesque nec iaculis metus.
+                </p>},
+                image_url: 'rails.png',
+                price: 19.95)
+# . . .
+
+Product.create!(title: 'Rails REST API',
+                description:
+                    %{<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Curabitur in felis ipsum. Aliquam maximus elit aliquet, pellentesque velit in, vulputate metus.
+                    Phasellus rutrum gravida nibh, ac lobortis nibh faucibus vitae.
+                    Sed in consectetur ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                    Aliquam euismod gravida venenatis. Maecenas vitae ligula quis ex porta vehicula. Pellentesque nec iaculis metus.
+                </p>},
+                image_url: 'rest_API.png',
+                price: 24.95)
